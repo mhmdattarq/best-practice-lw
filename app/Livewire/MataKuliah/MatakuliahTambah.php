798e39c474035cd6be05c88a7eb3fab77c57ecff
atta <atarik.shen05@gmail.com>
2026-02-16 16:34:09 +0700

    penambahan modul Matakuliah

---
+++ app/Livewire/MataKuliah/MatakuliahTambah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\MataKuliah;
+
+use Livewire\Component;
+
+class MatakuliahTambah extends Component
+{
+    public function render()
+    {
+        return view('mods.mata_kuliah.matakuliah_tambah');
+    }
+}
