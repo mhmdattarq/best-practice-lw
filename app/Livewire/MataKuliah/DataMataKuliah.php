5a9068c86f5db7e2de042e57714e60957c428652
atta <atarik.shen05@gmail.com>
2026-02-16 16:45:57 +0700

    penambahan migration

---
+++ app/Livewire/MataKuliah/DataMataKuliah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Matakuliah;
+
+use Livewire\Component;
+
+class DataMataKuliah extends Component
+{
+    public function render()
+    {
+        return view('mods.matakuliah.data_mata_kuliah');
+    }
+}
